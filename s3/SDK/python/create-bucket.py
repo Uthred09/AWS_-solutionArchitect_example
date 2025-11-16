eba02bb70b1976ccd3531c727e63e7d2c9348279
Uthred09 <susanbashyal29@gmail.com>
2025-11-16 13:45:20 +0000

    Simple cfn s3 example

--- s3/SDK/python/create-bucket.py
+++ s3/SDK/python/create-bucket.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 
-
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
 def create_bucket(bucket_name, region='us-east-1'):
     # Create S3 client
     s3_client = boto3.client(
--- s3/SDK/python/delete-bucket.py
+++ s3/SDK/python/delete-bucket.py
@@ -1,6 +1,7 @@
 import boto3
 import os
 
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/delete_bucket.html
 def delete_bucket(bucket_name):
     # Create S3 client
     s3_client = boto3.client(
